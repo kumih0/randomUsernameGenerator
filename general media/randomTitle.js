c1ddbdab2bcdc466efa04eb47d80e699f766a949
KT <132851569+kumih0@users.noreply.github.com>
2023-09-18 16:03:32 -0500

    Started random title pcs, function make plural

--- general media/randomTitle.js
+++ general media/randomTitle.js
@@ -1,4 +1,30 @@
 //creating formula for titles of movies, books etc.
 const articles = [
     "the", "a", "an", 
-]
+];
+
+const mysNoun = [
+    "mystery", "secret", "treasure", "adventure", "journey", "quest", "travel", "history", "fantasy", "dream", "creation", 
+    "legend", "intrigue", "consequence", "exploration", "hunt", "world", "ballad", "song", "tale", "trick", "vision", "trial",
+    "shadow", "call", "dance", "memory", "code", "exploit", "story", "whisper", 
+    
+    ];
+
+const makeItPlural = (w) => {
+    const lastChar = w[w.length-1];
+    
+    if(lastChar === "y") {
+        w.pop();
+        w.push("ies");
+        
+        console.log(w);
+        return w;
+    } else {
+        w.push("s");
+        
+        console.log(w);
+        return w;
+    };
+};
+
+const 
