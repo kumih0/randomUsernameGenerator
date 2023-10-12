871c0c67e6835cb5af929dab6bd7bbabf821601c
KT <132851569+kumih0@users.noreply.github.com>
2023-10-12 10:56:37 -0500

    Add prep, poss, place to rantit

--- general media/randomTitle.js
+++ general media/randomTitle.js
@@ -5,10 +5,29 @@ const articles = [
 
 const mysNoun = [
     "mystery", "secret", "treasure", "adventure", "journey", "quest", "travel", "history", "fantasy", "dream", "creation", 
-    "legend", "intrigue", "consequence", "exploration", "hunt", "world", "ballad", "song", "tale", "trick", "vision", "trial",
-    "shadow", "call", "dance", "memory", "code", "exploit", "story", "whisper", 
+    "legend", "intrigue", "consequence", "exploration", "hunt", "ballad", "song", "tale", "trick", "vision", "trial",
+    "shadow", "call", "dance", "memory", "code", "exploit", "story", "whisper", "dimension", 
     
     ];
+    
+const place = [
+    "world", "land", "sea", "ocean", "beach", "place", "city", "country", "town", "coast", "valley", "mountain", "forest",
+    "village", "river", "creek", "lake", "wood", "jungle", "street", "hill", "field", "beach", "garden", "meadow", "road",
+    "house", "building", "apartment", "cabin", "farm", "home", "office", "barn", "prison", "jail", "church", "shop", "store", 
+    "downtown", "uptown", "basement", "closet", "kitchen", "bedroom", "bathroom", "shower", "camp", "school", "class", "gym",
+    "pool", "toilet", "room", "library", 
+    ];
+    
+
+
+const prepositions = [
+    "for", "to", "of", "in", "by", "above", "below", "with", "without", "on", "onto", "into", "around", "about", "across", "towards", "against", "along", "among", "before", 
+    "behind", "beside", "from", "beneath", "between", "down", "under", "over", "near", "off", "within",
+    ];
+
+const dramAd = [
+    "deep", "lost", 
+];
 
 const makeItPlural = (w) => {
     const lastChar = w[w.length-1];
@@ -27,4 +46,8 @@ const makeItPlural = (w) => {
     };
 };
 
-const 
+const makeItPosessive = (w) => {
+    return w + "'s";
+};
+
+    
