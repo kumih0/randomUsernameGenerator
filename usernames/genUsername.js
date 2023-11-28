b6f286e5d3feb469c22c88ac1430aa6abeb6d5a5
KT <132851569+kumih0@users.noreply.github.com>
2023-11-28 11:29:04 -0600

    randomadjectivearray based on input

--- usernames/genUsername.js
+++ usernames/genUsername.js
@@ -1,6 +1,7 @@
 // const randomAdjectiveArray = require('./randomAdjective');
 // const randomNounArray = require('./randomNoun');
-const { nouns, dookieNouns } = require('../utils/randomNounList')
+const { nouns, dookieNouns } = require('../utils/randomNounList');
+const { adjectives, dookieAdj } = require('../utils/randomAdjectiveList');
 const { getRandomArrayItem, checkRepeats } = require('../utils/helpers');
 
 //create random noun array based on input
@@ -12,9 +13,18 @@ const randomNounArray = (ans) => {
     };
 };
 
+//create random adj array based on input
+const randomAdjectiveArray = (ans) => {
+    if (ans) {
+        return adjectives.concat(dookieAdj);
+    } else {
+        return adjectives;
+    };
+};
+
 //generate single username
 const generateRandomUsername = (ans) => {
-    return getRandomArrayItem(randomAdjectiveArray)+getRandomArrayItem(randomNounArray(ans))+(Math.floor(Math.random() * 100));
+    return getRandomArrayItem(randomAdjectiveArray(ans))+getRandomArrayItem(randomNounArray(ans))+(Math.floor(Math.random() * 100));
 };
 
 //generate list of usernames, req number input
@@ -22,12 +32,12 @@ const generateRandomUsernameList = (ans, num) => {
     //empty usernames array
     const usernames = [];
     for (let i = 0; i < num; i++) {
-        let randomAdj = getRandomArrayItem(randomAdjectiveArray);
+        let randomAdj = getRandomArrayItem(randomAdjectiveArray(ans));
         let randomNoun = getRandomArrayItem(randomNounArray(ans));
 
         console.log(randomAdj, randomNoun);
 
-        randomAdj = checkRepeats(usernames, randomAdjectiveArray, randomAdj);
+        randomAdj = checkRepeats(usernames, randomAdjectiveArray(ans), randomAdj);
         randomNoun = checkRepeats(usernames, randomNounArray(ans), randomNoun);
 
         const newUsername = randomAdj + randomNoun + (Math.floor(Math.random() * 100));
